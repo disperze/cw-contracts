54e2c3ca58d05b4b8ca430cf73e17280fb21800a
Giancarlos Salas <giansalex@gmail.com>
2021-07-21 16:27:38 -0500

    Add cw20 msgs

--- src/msg.rs
+++ src/msg.rs
@@ -1,13 +1,18 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::Uint128;
-use cw20::Cw20ReceiveMsg;
+use cosmwasm_std::{Uint128, Binary};
+use cw20::{Cw20ReceiveMsg, Expiration};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
+    /// native coin to wrap
     pub native_coin: String,
-    pub min_mint: Uint128,
+
+    // CW20
+    pub name: String,
+    pub symbol: String,
+    pub decimals: u8,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -17,12 +22,67 @@ pub enum ExecuteMsg {
     Deposit {},
     /// Withdraw your wrapped token previous allowance
     Withdraw { amount: Uint128 },
+
+    // Implements CW20
+    /// CW20. Transfer is a base message to move tokens to another account without triggering actions
+    Transfer { recipient: String, amount: Uint128 },
+    /// CW20. Burn is a base message to destroy tokens forever
+    Burn { amount: Uint128 },
+    /// CW20. Send is a base message to transfer tokens to a contract and trigger an action
+    /// on the receiving contract.
+    Send {
+        contract: String,
+        amount: Uint128,
+        msg: Binary,
+    },
+    /// CW20 "approval" extension. Allows spender to access an additional amount tokens
+    /// from the owner's (env.sender) account. If expires is Some(), overwrites current allowance
+    /// expiration with this one.
+    IncreaseAllowance {
+        spender: String,
+        amount: Uint128,
+        expires: Option<Expiration>,
+    },
+    /// CW20 "approval" extension. Lowers the spender's access of tokens
+    /// from the owner's (env.sender) account by amount. If expires is Some(), overwrites current
+    /// allowance expiration with this one.
+    DecreaseAllowance {
+        spender: String,
+        amount: Uint128,
+        expires: Option<Expiration>,
+    },
+    /// CW20 "approval" extension. Transfers amount tokens from owner -> recipient
+    /// if `env.sender` has sufficient pre-approval.
+    TransferFrom {
+        owner: String,
+        recipient: String,
+        amount: Uint128,
+    },
+    /// CW20 "approval" extension. Sends amount tokens from owner -> contract
+    /// if `env.sender` has sufficient pre-approval.
+    SendFrom {
+        owner: String,
+        contract: String,
+        amount: Uint128,
+        msg: Binary,
+    },
+    /// CW20 "approval" extension. Destroys tokens forever
+    BurnFrom { owner: String, amount: Uint128 },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
     Info {},
+
+    // Implements CW20
+    /// CW20. Returns the current balance of the given address, 0 if unset.
+    Balance { address: String },
+    /// CW20. Returns metadata on the contract - name, decimals, supply, etc.
+    TokenInfo {},
+    /// CW20 "allowance" extension.
+    /// Returns how much spender can use from owner account, 0 if unset.
+    Allowance { owner: String, spender: String },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
