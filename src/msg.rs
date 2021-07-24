06d47adaaac243977d0f2ccae870e21554ff6f59
Giancarlos Salas <giansalex@gmail.com>
2021-07-24 10:49:47 -0500

    Add lock, unlock msg

--- src/msg.rs
+++ src/msg.rs
@@ -1,27 +1,36 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use cosmwasm_std::{Timestamp, Coin};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
-    pub count: i32,
+    /// Max lock time in seconds
+    pub max_lock_time: u64,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    Increment {},
-    Reset { count: i32 },
+    /// Lock funds until expire timestamp
+    Lock {
+        expire: Timestamp
+    },
+    /// Unlock funds
+    Unlock {},
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    GetCount {},
+    /// Returns the lock info
+    GetLock { address: String },
 }
 
 // We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
+pub struct LockResponse {
+    pub start: Timestamp,
+    pub end: Timestamp,
+    /// Amount locked in native tokens
+    pub amount: Vec<Coin>,
 }
