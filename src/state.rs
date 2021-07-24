19a0a4c9b73870a53d7125125a4a1c4b3298acca
Giancarlos Salas <giansalex@gmail.com>
2021-07-24 10:49:18 -0500

    Update state props

--- src/state.rs
+++ src/state.rs
@@ -2,12 +2,14 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cw_storage_plus::{Item, Map};
+use crate::msg::LockResponse;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
-    pub count: i32,
+    pub max_lock_time: u64,
     pub owner: Addr,
 }
 
 pub const STATE: Item<State> = Item::new("state");
+pub const LOCKS: Map<&Addr, LockResponse> = Map::new("locks");
