a90594a8c15e6535a4edbbeee0a908425640825a
Giancarlos Salas <giansalex@gmail.com>
2021-07-25 16:24:32 -0500

    Impl add_token GenericBalance

--- src/state.rs
+++ src/state.rs
@@ -17,6 +17,41 @@ pub struct GenericBalance {
     pub cw20: Vec<Cw20CoinVerified>,
 }
 
+impl GenericBalance {
+    pub fn add_tokens(&mut self, add: Balance) {
+        match add {
+            Balance::Native(balance) => {
+                for token in balance.0 {
+                    let index = self.native.iter().enumerate().find_map(|(i, exist)| {
+                        if exist.denom == token.denom {
+                            Some(i)
+                        } else {
+                            None
+                        }
+                    });
+                    match index {
+                        Some(idx) => self.native[idx].amount += token.amount,
+                        None => self.native.push(token),
+                    }
+                }
+            }
+            Balance::Cw20(token) => {
+                let index = self.cw20.iter().enumerate().find_map(|(i, exist)| {
+                    if exist.address == token.address {
+                        Some(i)
+                    } else {
+                        None
+                    }
+                });
+                match index {
+                    Some(idx) => self.cw20[idx].amount += token.amount,
+                    None => self.cw20.push(token),
+                }
+            }
+        };
+    }
+}
+
 impl From<Balance> for GenericBalance {
     fn from(balance: Balance) -> GenericBalance {
         match balance {
