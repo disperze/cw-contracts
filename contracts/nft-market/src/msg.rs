cc1fcb5729bbd347bb47635e8573efc6b16b4292
Giancarlos Salas <giansalex@gmail.com>
2021-08-27 12:04:42 -0500

    Add market msgs

--- contracts/nft-market/src/msg.rs
+++ contracts/nft-market/src/msg.rs
@@ -1,27 +1,26 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use cw721::Cw721ReceiveMsg;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
-    pub count: i32,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    Increment {},
-    Reset { count: i32 },
+    WithdrawNft { offering_id: String },
+    ReceiveNft(Cw721ReceiveMsg),
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
     GetCount {},
 }
 
 // We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct CountResponse {
-    pub count: i32,
+    pub count: u64,
 }
