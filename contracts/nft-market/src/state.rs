302d307e47433d55ffe541ff9e9d9e4afcb08b51
Giancarlos Salas <giansalex@gmail.com>
2021-08-27 12:02:32 -0500

    Add offering to state

--- contracts/nft-market/src/state.rs
+++ contracts/nft-market/src/state.rs
@@ -1,13 +1,23 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cosmwasm_std::{Addr, CanonicalAddr, Coin};
+use cw_storage_plus::{Item, Map};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
     pub count: i32,
+    pub num_offerings: u64,
     pub owner: Addr,
 }
 
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct Offering {
+    pub token_id: String,
+    pub contract: Addr,
+    pub seller: Addr,
+    pub list_price: Coin,
+}
+
 pub const STATE: Item<State> = Item::new("state");
+pub const OFFERINGS: Map<&str, Offering> = Map::new("offerings");
